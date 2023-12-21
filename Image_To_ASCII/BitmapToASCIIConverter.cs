45fc441118eb484d85a84f3b45e2b9d35aa329a7
DigitalCatharsis <shinegumy@gmail.com>
2023-12-21 11:41:58 +0300

    Initial commit

---
+++ Image_To_ASCII/BitmapToASCIIConverter.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Image_To_ASCII
+{
+    public class BitmapToASCIIConverter
+    {
+        private readonly char[] _asciiTable = { '.', ',', ':', '+', '*', '?', '%', 'S', '#', '@' };   //Яркость по возрастанию, эдакий градиент
+        private readonly char[] _asciiTableNegative = { '@', '#', 'S', '%', '?', '*', '+', ':', ',', '.' };   //Яркость по возрастанию, эдакий градиент
+        private readonly Bitmap _bitmap;
+
+        public BitmapToASCIIConverter(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+        }
+
+        public char[][] Convert()
+        {
+            return Convertation(_asciiTable);
+        }
+        public char[][] ConvertAsNegative()
+        {
+            return Convertation(_asciiTableNegative);
+        }
+
+        private char[][] Convertation(char[] _asciiTable)
+        {
+            var result = new char[_bitmap.Height][];
+
+            for (int y = 0; y < _bitmap.Height; y++)
+            {
+                result[y] = new char[_bitmap.Width];
+                for (int x = 0; x < _bitmap.Width; x++)
+                {
+                    int mapIndex = (int)Map(_bitmap.GetPixel(x, y).R, 0, 255, 0, _asciiTable.Length - 1);
+                    result[y][x] = _asciiTable[mapIndex];
+                }
+            }
+            return result;
+        }
+
+        //Мапим значение из одного диапозона в другой (яркость)
+        //start/stop 1 - диапозоны 0 - 255 пикселей в битпаме 
+        //start/stop 2 - диаозон _asciiTable 
+        private float Map(float valueToMap, float start1, float stop1, float start2, float stop2) 
+        {
+            return ((valueToMap - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+    }
+}
---
+++ Image_To_ASCII/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+using Image_To_ASCII;
+using System.IO;
+
+// настройки шрифтов консоли 8 consola
+
+const double widthOffset = 2;  //фиксация размеров шрифта
+const int maxWidth = 200;  //Разрешение  //474 is max
+static Bitmap ResizeBitmap(Bitmap bitmap)
+{
+    var newHeight = bitmap.Height / widthOffset * maxWidth / bitmap.Width;
+    if (bitmap.Width > maxWidth || bitmap.Height > newHeight)
+    {
+        bitmap = new Bitmap(bitmap, new Size(maxWidth, (int) newHeight));
+    }
+    return bitmap;
+}
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+//Console.BackgroundColor = ConsoleColor.Black;
+//Console.ForegroundColor = ConsoleColor.White;
+
+string path = "C:\\Temp\\1111111.jpg";
+
+while (true)
+{
+    Console.ReadLine();
+
+    if (path == null)
+    {
+        continue;
+    }
+
+    Console.Clear();
+
+    var bitmap = new Bitmap(path);
+    bitmap = ResizeBitmap(bitmap);
+    bitmap.ToGrayScale();
+
+    var converter = new BitmapToASCIIConverter(bitmap);
+    var rows = converter.Convert();
+
+    foreach (var row in rows)
+    {
+        Console.WriteLine(row);
+    }
+
+    var rowsNegative = converter.ConvertAsNegative();
+    File.WriteAllLines("Image.Txt", rowsNegative.Select(r=> new string(r)));
+
+    Console.SetCursorPosition(0,0);
+
+
+
+}
+
